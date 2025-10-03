c5dd12b3e6e272fcc271a906b41da12802198637
Ujjwal Kumar Dhiraj <ujjwall2000@gmail.com>
2025-10-03 20:58:37 +0530

    Update apps.py for running the scrapper every hour

--- newsapp/apps.py
+++ newsapp/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+from apscheduler.schedulers.background import BackgroundScheduler
+from scraper.scraper import GenericNewsScraper
 
+def start_scraper_job():
+    scraper = GenericNewsScraper()
+    scraper.run_all_scrapers()
 
 class NewsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'newsapp'
+
+    def ready(self):
+        if 'runserver' in __import__('sys').argv or 'gunicorn' in __import__('sys').argv:
+            scheduler = BackgroundScheduler()
+            scheduler.add_job(start_scraper_job, 'interval', hours=1)
+            scheduler.start()
