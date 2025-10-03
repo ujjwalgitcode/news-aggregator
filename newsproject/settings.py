f452df235c1ab9f76528b8bee29e787da3fc5eab
Ujjwal Kumar Dhiraj <ujjwall2000@gmail.com>
2025-10-03 15:45:43 +0530

    secret key changes

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jl75z*-b)_py3%m)&o^7y(8v6_z^egcl8xos^u$hpb)1&x3h3)'
+# SECRET_KEY = 'django-insecure-jl75z*-b)_py3%m)&o^7y(8v6_z^egcl8xos^u$hpb)1&x3h3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG", "False") == "True"
